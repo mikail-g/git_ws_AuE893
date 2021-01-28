bcd36604cbbe97b0848fe4c113efe717388fe384
Mikaila Gossman <mikailg@g.clemson.edu>
2021-01-28 14:33:40 -0500

    Add screenshots for README because I forgot we also needed pics

--- Assignment2/scripts/square_openloop.py
+++ Assignment2/scripts/square_openloop.py
@@ -3,17 +3,32 @@ import rospy
 from geometry_msgs.msg import Twist
 PI = 3.1415926535897
 
-def move():
-	rospy.init_node('robot_cleaner', anonymous=True)
+
+
+def rotate(angle):
+		velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+		vel_msg = Twist()
+		vel_msg.linear.x = 0
+		vel_msg.angular.z = 0.2
+		rel_angle = angle * 2 * PI/360
+		traveled = 0
+		t0 = rospy.Time.now().to_sec()
+		while(traveled < rel_angle) :
+			velocity_publisher.publish(vel_msg)
+			t1 = rospy.Time.now().to_sec()
+			traveled = vel_msg.angular.z * (t1 - t0)	
+		
+		vel_msg.linear.x = 0
+		vel_msg.angular.z = 0
+		velocity_publisher.publish(vel_msg)
+
+def straightline(dist):
 	velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 	vel_msg = Twist()
-	
-	
-	#step 1:
 	vel_msg.linear.x = 0.2
 	vel_msg.angular.z = 0
 	
-	distance = 2
+	distance = dist
 	traveled = 0
 	t0 = rospy.Time.now().to_sec()
 	while(traveled < distance):
@@ -24,103 +39,17 @@ def move():
 	vel_msg.linear.x = 0
 	vel_msg.angular.z = 0
 	velocity_publisher.publish(vel_msg)
-		
-	#step 2:
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0.2
-	
-	rel_angle = 90 * 2 * PI/360
-	traveled = 0
-	t0 = rospy.Time.now().to_sec()
-	while(traveled < rel_angle):
-		velocity_publisher.publish(vel_msg)
-		t1 = rospy.Time.now().to_sec()
-		traveled = vel_msg.angular.z * (t1 - t0)
-		
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0
-	velocity_publisher.publish(vel_msg)
-	
-	#step 3:
-	vel_msg.linear.x = 0.2
-	vel_msg.angular.z = 0
-	distance = 2
-	traveled = 0
-	t0 = rospy.Time.now().to_sec()
-	while(traveled < distance):
-		velocity_publisher.publish(vel_msg)
-		t1 = rospy.Time.now().to_sec()
-		traveled = vel_msg.linear.x * (t1 - t0)
-	
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0
-	velocity_publisher.publish(vel_msg)
-	
-	#step 4:
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0.2
-	
-	rel_angle = 90 * 2 * PI/360
-	traveled = 0
-	t0 = rospy.Time.now().to_sec()
-	while(traveled < rel_angle):
-		velocity_publisher.publish(vel_msg)
-		t1 = rospy.Time.now().to_sec()
-		traveled = vel_msg.angular.z * (t1 - t0)
-		
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0
-	velocity_publisher.publish(vel_msg)
-	
-	#Step 5:
-	vel_msg.linear.x = 0.2
-	vel_msg.angular.z = 0
-	distance = 2
-	traveled = 0
-	t0 = rospy.Time.now().to_sec()
-	while(traveled < distance):
-		velocity_publisher.publish(vel_msg)
-		t1 = rospy.Time.now().to_sec()
-		traveled = vel_msg.linear.x * (t1 - t0)
-	
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0
-	velocity_publisher.publish(vel_msg)
-	
-	#step 6:
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0.2
-	
-	rel_angle = 90 * 2 * PI/360
-	traveled = 0
-	t0 = rospy.Time.now().to_sec()
-	while(traveled < rel_angle):
-		velocity_publisher.publish(vel_msg)
-		t1 = rospy.Time.now().to_sec()
-		traveled = vel_msg.angular.z * (t1 - t0)
-		
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0
-	velocity_publisher.publish(vel_msg)
-	
-	#step 7:
-	vel_msg.linear.x = 0.2
-	vel_msg.angular.z = 0
-	distance = 2
-	traveled = 0
-	t0 = rospy.Time.now().to_sec()
-	while(traveled < distance):
-		velocity_publisher.publish(vel_msg)
-		t1 = rospy.Time.now().to_sec()
-		traveled = vel_msg.linear.x * (t1 - t0)
-	
-	vel_msg.linear.x = 0
-	vel_msg.angular.z = 0
-	velocity_publisher.publish(vel_msg)
-	
+
 	
 		
 if __name__ == '__main__':
 	try:
-		move()
+		rospy.init_node('robot_cleaner', anonymous=True)
+		straightline(2)
+		rotate(90)
+		straightline(2)
+		rotate(90)
+		straightline(2)
+		rotate(90)
+		straightline(2)
 	except rospy.ROSInterruptException: pass
